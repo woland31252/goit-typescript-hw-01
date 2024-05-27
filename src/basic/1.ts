55828a84051960cb4d664968cb29d43238a816c2
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-05-27 17:35:10 +0300

    fix basic_1.ts

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -2,7 +2,7 @@ const age: number = 50;
 const username: string = 'Max';
 const toggle: boolean = true;
 const empty: null = null;
-const callback = (a: number) => { return 100 + a };
+const callback = (a: number) :number => { return 100 + a };
 
 
 
